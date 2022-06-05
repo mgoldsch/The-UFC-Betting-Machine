1e7490d5b35692157f1f29badff7996f24be0f7e
Michael Goldschlager <michaelgoldschlager@Michaels-MacBook-Air.local>
2022-06-05 11:07:27 -0400

    added scrape functionality and removed uncessarcy import

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,14 @@
 import subprocess
 
+print("upcoming scrape started")
+
 upcoming = subprocess.run(["scrapy", "crawl", "upcoming"])
+
 print("upcoming scrape complete")
+
+
+print("ufcFights scrape started")
+
+fight_info = subprocess.run(["scrapy", "crawl", "ufcFights"])
+
+print("ufcFights scrape complete")
