8ca57b4a6b5e12fd154091b87be160b68f727468
Michael Goldschlager <michae@goldschlager.net>
2022-06-12 19:08:35 -0400

    got it working with twitter

--- tweeter/tweeter.py
+++ tweeter/tweeter.py
@@ -12,10 +12,8 @@ def create_api():
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
     try:
         api.verify_credentials()
     except Exception as e:
@@ -45,4 +43,4 @@ api = create_api()
 fight_predictions = pd.read_csv("/fight_prediction/fight_prediction.csv")
 
 #tweet for each fight prediction
-fight_predictions.apply(tweet_fight_pred, axis = 1, args = (api))
+fight_predictions.apply(lambda x: tweet_fight_pred(x, api), axis = 1)
