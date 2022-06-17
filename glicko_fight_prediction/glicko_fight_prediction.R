f2a130060b70131ad6296e9c94a77f685439af7f
Michael Goldschlager <michae@goldschlager.net>
2022-06-17 02:16:07 -0400

    fixed the prediction R script to actually do what it was intended to do

--- glicko_fight_prediction/glicko_fight_prediction.R
+++ glicko_fight_prediction/glicko_fight_prediction.R
@@ -14,26 +14,26 @@ ufc_upcoming <- read.csv(paste0("/data/upcoming/", ufc_upcoming_file))[, c("figh
 colnames(ufc_upcoming)[1] <- "R_fighter"
 colnames(ufc_upcoming)[2] <- "B_fighter"
 
-apply(ufc_upcoming, MARGIN = 1, fight_prediction)
+# fight_prediction_df <- data.frame(date = character(), 
+#                                   R_fighter = character(), 
+#                                   B_fighter = character(), 
+#                                   fight_id = character(), 
+#                                   location = character(), 
+#                                   weight_class = character(), 
+#                                   R_fighter_pred_win_prob = double(), 
+#                                   B_fighter_pred_win_prob = double())
 
-fight_prediction_df <- data.frame(date = character(), 
-                                  R_fighter = character(), 
-                                  B_fighter = character(), 
-                                  fight_id = character(), 
-                                  location = character(), 
-                                  weight_class = character(), 
-                                  R_fighter_pred_win_prob = double(), 
-                                  B_fighter_pred_win_prob = double())
-
-write.csv(fight_prediction_df, "/fight_prediction/fight_prediction.csv", row.names = FALSE)
-
-fight_prediction <- function(row){
-    fight <- row["date", "R_fighter", "B_fighter"]
+fight_prediction <- function(row) {
+    fight <- data.frame(t(row))[, c("date", "R_fighter", "B_fighter")]
     fight$date <- as.numeric(fight$date)
 
     R_fighter_pred_win_prob <- predict(glicko_rating, fight, tng = 1, trat = c(1500, 290))
     B_fighter_pred_win_prob <- predict(glicko_rating, fight[, c(1, 3, 2)], tng = 1, trat = c(1500, 290))
 
-    fight_pred <- cbind(row, R_fighter_pred_win_prob, B_fighter_pred_win_prob)
-    rbind(fight_prediction_df, fight_pred)
+    fight_pred <- cbind(data.frame(t(row)), R_fighter_pred_win_prob, B_fighter_pred_win_prob)
+    return(fight_pred)
 }
+
+fight_prediction_df <- do.call("rbind", apply(ufc_upcoming, MARGIN = 1, fight_prediction))
+
+write.csv(fight_prediction_df, "/fight_prediction/fight_prediction.csv", row.names = FALSE)
